e3f1b6ec6664813b192563857a77b00e36079eff
Eyre Xi <109657355+Eyre-dutchess@users.noreply.github.com>
2025-06-16 06:50:26 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 
 import { getAllListings, ISearchParams } from "./action/getAllListings";
 import { getCurrentUser } from "./action/getCurrentUser";
@@ -24,7 +25,9 @@ export default async function Home({params}: {params: Promise<ISearchParams>}) {
       <div className="fixed top-0 left-0 w-screen h-[50vh]  z-0  bg-gradient-to-b from-orange-600/75 via-blue-600/75 to-blue-100/5"></div>
       <Container>
           <HeaderSec />
-          <MainSec listings={listings} currentUser={currentUser}/>
+          <Suspense fallback={<Loading />}>
+            <MainSec listings={listings} currentUser={currentUser}/>
+          </Suspense>
           <Footer />
       </Container>
     </ClientOnly>
